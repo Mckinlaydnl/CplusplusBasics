61481eca127465a847fffd3b434e81c533c3ef6e
Mckinlaydnl <43002081+Mckinlaydnl@users.noreply.github.com>
2018-09-05 14:46:47 +0100

    Changed program

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,16 @@
 
-//Entry print for our program
+//Include Librabries
+#include <stdlib.h> //standard library
+#include <iostream>
+//Entry point for our program
 //Every C++ program needs a main() function
-int name()
+int main()
 {
-	//Tell the operating system we are done
-	//and there was no error
+	std::cout << "Hello world"<< std::endl;
+	//Wait for user to press button
+	system("PAUSE");
+
+	//Tell the operating system we are done and there was no error
 	return 0;
 }
 
